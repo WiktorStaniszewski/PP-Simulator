ee35c2eba029e569fb7441fd144099c53c5d4b65
WiktorStaniszewski <wiktorst.8@gmail.com>
2024-10-25 18:04:13 +0200

    Stwory

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -8,4 +8,20 @@ namespace Simulator;
 
 internal class Creature
 {
+    public string ?Name { get; set; }
+    public int Level { get; set; } = 1;
+    public string Info => $"{Name} [{Level}]";
+    
+    public Creature() { }
+    public Creature(string name, int level) 
+    {
+        Name = name;
+        Level = level;
+    }
+    
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
 }
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,5 +5,7 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting simulator!\n");
+        Creature c = new Creature("Mike Wazowski", 2);
+        c.SayHi();    
     }
 }
