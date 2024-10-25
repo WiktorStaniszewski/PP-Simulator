da8bdebdd1d36f6d07363a65d89f1301910adbd2
WiktorStaniszewski <wiktorst.8@gmail.com>
2024-10-25 23:01:38 +0200

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -8,7 +8,24 @@ namespace Simulator;
 
 internal class Animals
 {
-    public required string Description { get; init; }
+    private string description = "";
+    public required string Description
+    {
+        get => description;
+        init
+        {
+            string temp_desc = value;
+            if (temp_desc == null) return;
+            temp_desc = temp_desc.Trim();
+            while (temp_desc.Length < 3) temp_desc += "#";
+            if (temp_desc.Length > 15) temp_desc = temp_desc[..15];
+            if (!Char.IsUpper(temp_desc[0])) {
+                temp_desc = Char.ToUpper(temp_desc[0]) + temp_desc.Substring(1);
+            }
+            while (temp_desc.Length < 3) temp_desc += "#";
+            description = temp_desc.Trim();
+        }
+    }
     public uint Size { get; set; } = 3;
-    public string Info() => $"{Description} <{Size}>";
+    public string Info => $"{Description} <{Size}>";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -6,14 +6,42 @@ using System.Threading.Tasks;
 
 namespace Simulator;
 
-internal class Creature
+public class Creature
 {
-    public string ?Name { get; set; }
-    public int Level { get; set; } = 1;
-    public string Info => $"{Name} [{Level}]";
-    
+    private string name = "Unknown";
+    public string Name
+    {
+        get => name;
+        init
+        {
+            string temp_name = value;
+            if (temp_name == null) return;
+            temp_name = temp_name.Trim();
+            while (temp_name.Length < 3) temp_name += "#";
+            if (temp_name.Length > 25) temp_name = temp_name[..25].Trim();
+            if (!Char.IsUpper(temp_name[0])) {
+                temp_name = Char.ToUpper(temp_name[0]) + temp_name.Substring(1);
+            }
+            while (temp_name.Length < 3) temp_name += "#";
+            name = temp_name;
+        }
+    }
+        
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        init
+        {
+            int temp_int = value;
+            if (temp_int < 1) temp_int = 1;
+            else if (temp_int > 10) temp_int = 10;
+            level = temp_int;
+        }
+    }
+
     public Creature() { }
-    public Creature(string name, int level) 
+    public Creature(string name, int level = 1) 
     {
         Name = name;
         Level = level;
@@ -21,7 +49,13 @@ internal class Creature
     
     public void SayHi()
     {
-        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        Console.WriteLine($"Hi, I'm {Name}, my level is {level}.");
     }
-
+    public void Upgrade()
+    {
+        if (level != 10) level += 1;
+        else return;
+        
+    }
+    public string Info => $"{Name} [{level}]";
 }
